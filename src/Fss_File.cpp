7c77ea1185216d9faf35f5271c54ca0319ef9d3a
Chris <chriswhoprograms@gmail.com>
2024-02-06 20:51:13 -0500

    Changes to Fss_File and cleared a warning in test.cpp

--- src/Fss_File.cpp
+++ src/Fss_File.cpp
@@ -1,5 +1,5 @@
 #include "Fss_File.hpp"
 
 Fss_File::Fss_File(std::string fullPath, bool isDirectory, std::string VolumeTag, std::vector<int> checksum, std::filesystem::file_time_type lastModified, bool fromDb) : fullPath(fullPath), isDirectory(isDirectory), VolumeTag(VolumeTag), checksum(checksum), lastModified(lastModified), fromDb(fromDb){
-
+    
 }
